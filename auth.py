f1cc2d6959316b888a9ead406ae21628887f44de
Liz Smart <northwest@codeclub.org.uk>
2017-04-04 21:27:46 +0100

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,4 @@
+access_token = '3301545975-2Qbc2VTOxkX7GQzJAplKcgB253J8dgU06zWsrCi'
+access_token_secret = '9IaUSZFyvHSXrnoQF1W9p8Ift20XXLWjmghsFGGoe4lXa'
+consumer_key = 'f7r01tf1XeUVvtmMuOmP2WUFk'
+consumer_secret = 'nDk2opig9XLKB59nPozeFuJKgqJqFZnC3Sq52cS501KVBvxS1D'
